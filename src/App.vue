e7d7bdd7b36b1305866f53e47ab2b698fd177b77
AlexLoup33 <a.loupoueyou@icloud.com>
2025-12-13 16:49:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import HeaderVue from "./components/Header.vue"
+import FooterVue from "./components/Footer.vue"
+</script>
+
+<template>
+  <!-- HEADER -->
+  <HeaderVue />
+
+
+  <!-- APP -->
+  
+
+  <!-- FOOTER -->
+   <FooterVue />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import { createWebHistory, createRouter } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import("./components/Home.vue"),
+        props: true
+    },
+    {
+        path: "/work",
+        name: "work",
+        component: () => import("./components/Work.vue"),
+        props: true
+    },
+    {
+        path: "/blog",
+        name: "blog",
+        component: () => import ("./components/Blog.vue"),
+        props: true
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
